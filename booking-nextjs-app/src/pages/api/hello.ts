ba4c4807a953e198bf1437193ace36c30db82440
ioann7 <ioann.chimrov@gmail.com>
2025-03-02 20:58:03 +0300

    Добавлена интеграция с Turso + Prisma и добавлены основные модели.

--- booking-nextjs-app/src/pages/api/hello.ts
+++ booking-nextjs-app/src/pages/api/hello.ts
@@ -1,13 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/utils/prisma';
+import { Status } from '@prisma/client';
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse,
 ) {
-  res.status(200).json({ name: "John Doe" });
+  // Пример просмотра и создания. TODO удалить, после обкатки ORM.
+  if (req.query.method === 'get') {
+    res.status(200).json({ reservation: await prisma.reservation.findMany() })
+    return
+  }
+
+  const newReservation = await prisma.reservation.create({
+    data: {
+      data: {
+        user: 'John Doe',
+        flights: ['FL123', 'FL456'],
+        totalPrice: 200.5,
+      },
+      status: Status.IN_PROCESSING,
+    },
+  });
+
+  console.log('Reservation Created:', newReservation);
+  res.status(200).json({ message: 'Reservation created', reservation: newReservation });
 }
