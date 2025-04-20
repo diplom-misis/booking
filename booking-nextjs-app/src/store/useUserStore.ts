1f8658b4cba7fe1ebf6d8318163125688de299b8
ioann7 <ioannchimrov@gmail.com>
2025-04-20 15:55:53 +0300

    промежуточный коммит

--- booking-nextjs-app/src/store/useUserStore.ts
+++ booking-nextjs-app/src/store/useUserStore.ts
@@ -1,5 +1,7 @@
 import { create } from "zustand";
 
+// TODO убедиться, что не используется и удалить.
+
 interface User {
   id: string;
   email: string;
@@ -11,11 +13,29 @@ interface User {
 interface UserState {
   user: User | null;
   setUser: (user: User) => void;
+  setUserImage: (imageLink: string) => void;
   clearUser: () => void;
+  updateUser: (partialUser: Partial<User>) => void;
 }
 
 export const useUserStore = create<UserState>((set) => ({
   user: null,
   setUser: (user) => set({ user }),
+  setUserImage: (imageLink) =>
+    set((state) => {
+      if (!state.user) {
+        console.warn("Cannot update image - no user is set");
+        return {};
+      }
+      return { user: { ...state.user, imageLink } };
+    }),
   clearUser: () => set({ user: null }),
+  updateUser: (partialUser) =>
+    set((state) => {
+      if (!state.user) {
+        console.warn("Cannot update - no user is set");
+        return {};
+      }
+      return { user: { ...state.user, ...partialUser } };
+    }),
 }));
